e85d46a0c01cf681f3b55813fb4595fb5c8b522b
muhannd79 <muhannddh@gmail.com>
2020-02-13 17:47:02 -0600

    Add Converters

--- app/src/main/java/org/fooshtech/recipeslist/persistence/RecipeDatabase.java
+++ app/src/main/java/org/fooshtech/recipeslist/persistence/RecipeDatabase.java
@@ -6,11 +6,13 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import org.fooshtech.recipeslist.models.Recipe;
 
 
 @Database(entities = {Recipe.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class RecipeDatabase extends RoomDatabase {
 
     public static final String DATABASE_NAME = "recipes_db";
